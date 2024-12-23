d9ac52d4eaa9a581b099529fce38618bff4116e4
Vanessa <blackkoreangirl@gmail.com>
2024-12-23 14:39:01 -0500

    additional code comments and link to slide deck to fulfill all evaluation criteria

--- Python Friday Presentation/Vanessa/Week 2/main.py
+++ Python Friday Presentation/Vanessa/Week 2/main.py
@@ -14,3 +14,14 @@ def multiply_numbers(a, b):
     if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
         return "Error: Both inputs must be numbers"
     return a * b
+
+# Modified function for deliberate testing failure
+# def add_numbers(a, b):
+#     #if either of the inputs are not numbers (integers or floats) --> error message
+#     #otherwise execute calculations
+#     if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
+#         return "Error: Both inputs must be numbers"
+#     return 10
+
+#for additional evaluation criteria, please check presentation with screenshots starting at slide 7:
+#https://docs.google.com/presentation/d/1jO0z5rszhrV0PcIKiPuVCbq70r4P4tHZBG6tlzV0VRk/edit#slide=id.p
--- Python Friday Presentation/Vanessa/Week 2/test_main.py
+++ Python Friday Presentation/Vanessa/Week 2/test_main.py
@@ -6,17 +6,18 @@ from main import add_numbers, multiply_numbers
 class TestCalculations(unittest.TestCase):
             def test_add_numbers(self):
                 #assertion on outcome for test case examples
-                self.assertEqual(add_numbers(5, 3), 8)
-                self.assertEqual(add_numbers(-1, 1), 0)
+                self.assertEqual(add_numbers(5, 3), 8) #expecting output = 8 & OK (success)
+                self.assertEqual(add_numbers(-1, 1), 0) #expecting output = 0 & OK (success)
                 #edge case of string input: 
                 #self.assertEqual(add_numbers("three", 2), 5) 
 
             def test_multiply_numbers(self):
                 #assertion on outcome for test case examples
-                self.assertEqual(multiply_numbers(4, 5), 20)
-                self.assertEqual(multiply_numbers(-2, 3), -6)
-                #edge case of string iinput: 
-                #self.assertEqual(multiply_numbers("apples", 2), 3)
+                self.assertEqual(multiply_numbers(4, 5), 20) #expecting output = 20 & OK (success)
+                self.assertEqual(multiply_numbers(-2, 3), -6) #expecting output = -6 & OK (success)
+                
+                #edge case of string input: 
+                #self.assertEqual(multiply_numbers("apples", 2), 3) #expecting error message & failure
 
 if __name__ == '__main__':
                  unittest.main()
