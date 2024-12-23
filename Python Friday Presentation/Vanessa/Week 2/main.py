55a966fa42ea98aa69b309d0e54b7b64455519b3
Vanessa <blackkoreangirl@gmail.com>
2024-12-23 14:26:10 -0500

    adding code comments

--- Python Friday Presentation/Vanessa/Week 2/main.py
+++ Python Friday Presentation/Vanessa/Week 2/main.py
@@ -1,9 +1,16 @@
+#creating two basic functions (add_numbers, multiply_numbers) that take in numbers
+#handling edge case of non-numbers using 'if not... or not' with 'isinstance' to test for wrong data type
+
 def add_numbers(a, b):
+    #if either of the inputs are not numbers (integers or floats) --> error message
+    #otherwise execute calculations
     if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
         return "Error: Both inputs must be numbers"
     return a + b
 
 def multiply_numbers(a, b):
+    #if either of the inputs are not numbers (integers or floats) --> error message
+    #otherwise execute calculations
     if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
         return "Error: Both inputs must be numbers"
     return a * b
--- Python Friday Presentation/Vanessa/Week 2/test_main.py
+++ Python Friday Presentation/Vanessa/Week 2/test_main.py
@@ -1,14 +1,22 @@
+#testing functions (imported from main.py: add_numbers, multiply_numbers) using class (TestCalculations) importing unittest 'TestCase' method
+
 import unittest
 from main import add_numbers, multiply_numbers
 
 class TestCalculations(unittest.TestCase):
             def test_add_numbers(self):
+                #assertion on outcome for test case examples
                 self.assertEqual(add_numbers(5, 3), 8)
                 self.assertEqual(add_numbers(-1, 1), 0)
+                #edge case of string input: 
+                #self.assertEqual(add_numbers("three", 2), 5) 
 
             def test_multiply_numbers(self):
+                #assertion on outcome for test case examples
                 self.assertEqual(multiply_numbers(4, 5), 20)
                 self.assertEqual(multiply_numbers(-2, 3), -6)
+                #edge case of string iinput: 
+                #self.assertEqual(multiply_numbers("apples", 2), 3)
 
 if __name__ == '__main__':
                  unittest.main()
